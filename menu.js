3e277960ae1b658408ee48156a335474075e5c67
Rithin <rithinjohnsoncj@gmail.com>
2021-08-01 23:37:08 +0530

    initial commit

--- menu.js
+++ menu.js
@@ -1,47 +1,35 @@
 var count =1;
 function addData() {
-    var name = document.getElementById("name").value;
-    var age = document.getElementById("age").value; 
-    //alert("Event fired");
-    //This code is use to create the p tag
-    var myPTag = document.createElement("p");
-    //This code is use to create the content for any tags 
-    //var myPTagContent = document.createTextNode("Welcome to JavaScript");
-    var myPTagContent = document.createTextNode("Name is "+name+", Age is "+age);
-    // Adding the attribute for p tag
-    myPTag.setAttribute("style","color:white;font-size:24px;");
-    if(age>30){
-    myPTag.setAttribute("class","myClass");
-    } else {
-        myPTag.setAttribute("class","myClass1");    
-    }
-    myPTag.setAttribute("id",count);
-    var deleteNode = document.createElement("a");
-    var deleteNodeContent = document.createTextNode("X");
-    deleteNode.setAttribute("href","#");
-    deleteNode.setAttribute("onClick","deleteInfo("+count+")");
-    deleteNode.appendChild(deleteNodeContent);
-    //Add the cotent to p tag;
-    myPTag.appendChild(myPTagContent);
-    myPTag.appendChild(deleteNode);
+    var title = document.getElementById("Title").value;
+    var article = document.getElementById("Article").value; 
+    var Image = document.getElementById("Image").value; 
 
-    // <a href="#" onClick="deleteData()">X</a>
+    var myBox = document.createElement("div");
+    myBox.setAttribute("class","card m-3");
+   
+    var mytitle = document.createTextNode(title);
+    var DivTitle = document.createElement("div");
+    DivTitle.setAttribute("class","card-header");
+    DivTitle.appendChild(mytitle);
+
+    var myarticle = document.createTextNode(article);
+    var DivArticle = document.createElement("div");
+    DivArticle.setAttribute("class","card-clock-deals-detail");
+    DivArticle.appendChild(myarticle);
+
+    var DivImage = document.createElement("img");
+    DivImage.setAttribute("class","card-img-top");
+    DivImage.setAttribute("src",Image);
+    
+    myBox.appendChild(DivTitle);
+    myBox.appendChild(DivArticle);
+    myBox.appendChild(DivImage);
+
+   
+    myBox.setAttribute("style","color:black;font-size:16px;width: 180px;float: left;margin-left: 10px;");
+
     count++;
-    document.getElementById("main").appendChild(myPTag);
-    reset();
+    document.getElementById("box").appendChild(myBox);
+   
+   
 }
-
-function deleteInfo(count) {
-    console.log("delete fun called.."+count);
-    document.getElementById(count).remove();
-}
-
-function reset() {
-    document.getElementById("name").value="";
-    document.getElementById("age").value=""; 
-}
-
-/*
-<p style="color:red;font-size:24px" class="myClass">Name is Raj, Age is 21</p>
-
-*/
